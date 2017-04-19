99bb4b12a210080738d365a0317fabb7a36aff76
Prasang Sharma <Prasang Sharma>
2017-04-20 01:04:30 +0530

    first commit

---
+++ src/components/Chat.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Chat extends React.Component {
+	render() {
+		return(
+
+			<View>
+				<Text>
+					Hello Chat!	
+				</Text>
+			</View>
+
+		);
+	}
+}
+
+export default Chat;
---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Home extends React.Component {
+	render() {
+		return(
+
+			<View>
+				<Text>
+					Hello Home!	
+				</Text>
+			</View>
+
+		);
+	}
+}
+
+export default Home;
